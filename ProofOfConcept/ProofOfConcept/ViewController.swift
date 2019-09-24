8f36c6aaec35250e3db3911ba0dd257589a9900f
Carlos Pava <cpava@condorlabs.io>
2019-09-24 16:19:08 -0500

    update configuration

--- ProofOfConcept/ProofOfConcept/ViewController.swift
+++ ProofOfConcept/ProofOfConcept/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         let mediator = LogMediator()
         mediator.add(registerLog: ConsoleOSLog())
         mediator.add(registerLog: AnalitycsLog())
